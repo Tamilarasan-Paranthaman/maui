5472033b91eb0dd02c20753754f29ac192559298
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2025-08-25 18:05:18 +0530

    Fixed the Entry clear button color issue

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue31326.cs
@@ -0,0 +1,39 @@
+﻿namespace Maui.Controls.Sample.Issues;
+
+[Issue(IssueTracker.Github, 31326, "[iOS] Clear button does not apply TextColor when set at runtime", PlatformAffected.iOS)]
+public class Issue31326 : ContentPage
+{
+	public Issue31326()
+	{
+		var entry = new UITestEntry
+		{
+			Text = "Sample Entry",
+			IsCursorVisible = false,
+			ClearButtonVisibility = ClearButtonVisibility.WhileEditing,
+			AutomationId = "SampleEntry"
+		};
+
+		var button = new Button
+		{
+			Text = "Set Text Color to Red",
+			AutomationId = "SetTextColorButton"
+		};
+
+		button.Clicked += (sender, args) =>
+		{
+			entry.TextColor = Colors.Red;
+		};
+
+		var verticalStackLayout = new VerticalStackLayout
+		{
+			Spacing = 30,
+			Padding = 20,
+			Children =
+			{
+				entry,
+				button
+			},
+		};
+		Content = verticalStackLayout;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue31326.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue31326 : _IssuesUITest
+{
+	public override string Issue => "[iOS] Clear button does not apply TextColor when set at runtime";
+
+#if IOS
+	private const int CropBottomValue = 1550;
+#elif ANDROID
+	private const int CropBottomValue = 1150;
+#elif WINDOWS
+	private const int CropBottomValue = 400;
+#else
+	private const int CropBottomValue = 360;
+#endif
+
+	public Issue31326(TestDevice device)
+	: base(device)
+	{ }
+
+	[Test]
+	[Category(UITestCategories.Entry)]
+	public void VerifyEntryClearButtonColor()
+	{
+		App.WaitForElement("SetTextColorButton");
+		App.Tap("SetTextColorButton");
+		App.Tap("SampleEntry");
+		VerifyScreenshot(cropBottom: CropBottomValue);
+	}
+}
